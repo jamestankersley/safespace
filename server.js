7b11e1af5af64f80f1ad67f79848c1cc0b0d22c1
Christian <christianlance85@gmail.com>
2020-03-26 20:25:54 -0500

    server stuff

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8080;
-const db = require("models");
+const compression = require("compression");
+
+
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
-app.use(express.urlencoded({ extended: true}));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/safespace", {
   useNewUrlParser: true,
   useFindAndModify: false
