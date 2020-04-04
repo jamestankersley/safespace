7d9a002f9cb9197e5107f1e6e39ee82c202c67c2
Christian <christianlance85@gmail.com>
2020-04-04 11:11:01 -0500

    Standup

---
+++ src/post/api-post.js
@@ -0,0 +1,63 @@
+const create = (params, credentials, post) => {
+    return fetch('/api/posts/new/'+ params.userId, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: post
+    }).then((response) => {
+      return response.json()
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+  
+  const listByUser = (params, credentials) => {
+    return fetch('/api/posts/by/'+ params.userId, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    }).then(response => {
+      return response.json()
+    }).catch((err) => console.log(err))
+  }
+  
+  const listNewsFeed = (params, credentials) => {
+    return fetch('/api/posts/feed/'+ params.userId, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    }).then(response => {
+      return response.json()
+    }).catch((err) => console.log(err))
+  }
+  
+  const remove = (params, credentials) => {
+    return fetch('/api/posts/' + params.postId, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    }).then((response) => {
+      return response.json()
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  export {
+    listNewsFeed,
+    listByUser,
+    create,
+    remove
+  }
+  
