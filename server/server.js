9acf95f7adff7409fafc507fcce0a260b5d736be
Christian <christianlance85@gmail.com>
2020-04-16 20:30:54 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ mongoose.connect(config.mongoUri)
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 })
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/safespace";
-mongoose.connect(MONGODB_URI);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Headlines";
+mongoose.connect(MONGODB_URI)
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
